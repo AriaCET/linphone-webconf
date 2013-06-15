a25ae37c7dfa852dcccb5ea1ae46a408295b51f3
Dhananjay Balan <mb.dhananjay@gmail.com>
2013-06-15 07:33:12 +0530

    Added linphone backend

---
+++ linphone.py
@@ -0,0 +1,38 @@
+import os
+
+class Linphone:
+    def __init__(self):
+        self.state = False
+        try:
+            if(not os.system("linphonecsh init")):
+                print "Daemon inited"
+            else:
+                raise OSError
+            self.enable_autoanswer()
+            self.use_bcm_card()
+        except OSError:
+            print "E: Cant spin up the daemon"
+            exit()
+
+    def register(self,host,username,password):
+        try:
+            if(not os.system("linphonecsh register --host "+host+" --username "+username+" --password "+password)):
+                print  "I: Registred"
+                return True
+        except Error:
+            print "E: Registration Unsuccessful"
+            return False
+
+    def enable_autoanswer(self):
+        if(not os.system("linphonecsh generic 'autoanswer enable'")):
+            return True
+        return False
+
+    def use_bcm_card(self):
+        if(not os.system("linphonecsh soundcard ring 2")):
+            if(not os.system("linphonecsh soundcard playback 2")):
+                return True
+        return False
+
+    def __del__(self):
+        os.system("linphonecsh exit")
